c9a5348af8af59db323a32bb5a784318dabd2875
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-10 21:19:03 +0900

    chore, feat: threetenabp dependency 추가(내부적으로 사용), 토요일은 파란색, 일요일은 빨간색으로 데코 기능 추가

--- app/src/main/java/dev/ehyeon/calendarpracticeapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/calendarpracticeapplication/MainActivity.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.threeten.bp.LocalDate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
+
+        materialCalendarView.setSelectedDate(LocalDate.now());
+
+        materialCalendarView.addDecorators(new SaturdayViewDecorator(), new SundayViewDecorator());
     }
 }
